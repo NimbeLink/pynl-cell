b8dd082a6bd813086d287b73d96f9a301d4fbc2a
Erik Johnson <joh06937@gmail.com>
2021-04-22 16:26:40 -0500

    Skywire: Changed network mode to a property
    I'm hoping doing:
    
        skywire = Skywire()
        skywire.networkMode = NetworkMode.On
    
    is more intuitive than using setNetworkMode() and will make discovering all of
    the functionality available in a Skywire easier. I.e. you can look up modules
    within a Skywire and not have to keep track of the individual setX() and getX()
    APIs.
    
    Also changed the name of the AT interface to just 'at'. 'atInterface' is a
    little heavy, and 'at' will fit a little better with incoming sub-module design
    changes.

--- nl_cell/modem/skywire.py
+++ nl_cell/modem/skywire.py
@@ -18,20 +18,67 @@ class Skywire(object):
     """A Skywire modem
     """
 
-    def __init__(self, atInterface):
+    def __init__(self, at):
         """Creates a new Skywire modem
 
         :param self:
             Self
-        :param atInterface:
+        :param at:
             Our AT interface
 
         :return none:
         """
 
-        self.atInterface = atInterface
+        self._at = at
 
-    def setNetworkMode(self, networkMode):
+    @property
+    def at(self):
+        """Gets our AT interface
+
+        :param self:
+            Self
+
+        :return AtInterface:
+            Our AT interface
+        """
+
+        return self._at
+
+    @property
+    def networkMode(self):
+        """Gets the current network mode
+
+        :param self:
+            Self
+
+        :raise IOError:
+            Failed to get network mode
+
+        :return Network.Mode:
+            The current network mode
+        """
+
+        response = self.at.sendCommand("AT+CEREG?")
+
+        # If we failed to query the network mode, that's a paddlin'
+        if not response:
+            raise IOError("Failed to query network mode")
+
+        fields = response.output.split(",")
+
+        # If there isn't at least the prefix and the current mode, that's a
+        # paddlin'
+        if len(fields) < 2:
+            raise IOError("Invalid network mode response")
+
+        try:
+            return int(fields[1])
+
+        except ValueError:
+            raise IOError("Invalid network mode")
+
+    @networkMode.setter
+    def networkMode(self, networkMode):
         """Sets our network mode
 
         :param self:
@@ -39,75 +86,13 @@ class Skywire(object):
         :param networkMode:
             The network mode to set
 
-        :return True:
-            Network mode set
-        :return False:
+        :raise IOError:
             Failed to set network mode
+
+        :return none:
         """
 
         # Setting the network mode can take a bit of time, so give it 10 seconds
         # to finish
-        if not self.atInterface.sendCommand("AT+CFUN={}".format(networkMode), timeout = 10):
-            return False
-
-        return True
-
-    def getNetworkMode(self):
-        """Gets the current network mode
-
-        :param self;
-            Self
-
-        :return None:
-            Failed to get network mode
-        :return Network.Mode:
-            The current network mode
-        """
-
-        response = self.atInterface.sendCommand("AT+CEREG?")
-
-        # If we failed to query the network mode, that's a paddlin'
-        if not response:
-            return None
-
-        fields = response.output.split(",")
-
-        # If there isn't at least the prefix and the current mode, that's a
-        # paddlin'
-        if len(fields) < 2:
-            return None
-
-        try:
-            return int(fields[1])
-
-        except ValueError:
-            return None
-
-    def waitForNetworkMode(self, networkMode, timeout = None):
-        """Waits for the modem to reach a network mode
-
-        :param self:
-            Self
-        :param networkMode:
-            The network mode to wait for
-        :param timeout:
-            How long to wait
-
-        :return True:
-            Network mode reached
-        :return False:
-            Failed to read network mode before timeout
-        """
-
-        start = time.time()
-
-        while True:
-            # If it's been too long, that's a paddlin'
-            if (timeout != None) and ((time.time() - start) >= timeout):
-                return False
-
-            # If the modem is at the desired network mode, done
-            if self.getNetworkMode() == networkMode:
-                return True
-
-            time.sleep(2)
+        if not self.at.sendCommand("AT+CFUN={}".format(networkMode), timeout = 10):
+            raise IOError("Failed to set network mode")
