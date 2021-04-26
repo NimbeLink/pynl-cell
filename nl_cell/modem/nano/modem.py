94135a93a6ec8128d6c272137925eadaf49a5bcb
Erik Johnson <joh06937@gmail.com>
2021-04-26 13:28:29 -0500

    Skywire Nano: Added a new 'app' sub-module
    And moved the version getting into the new sub-module.

--- nl_cell/modem/nano/modem.py
+++ nl_cell/modem/nano/modem.py
@@ -15,6 +15,7 @@
 from nimbelink.cell.at.interface import AtInterface
 from nimbelink.cell.modem.skywire import Skywire
 
+from nimbelink.cell.modem.nano.app import App
 from nimbelink.cell.modem.nano.gpio import Gpio
 from nimbelink.cell.modem.nano.sim import Sim
 from nimbelink.cell.modem.nano.socket import Socket
@@ -40,11 +41,25 @@ class SkywireNano(Skywire):
 
         super(SkywireNano, self).__init__(*args, **kwargs)
 
+        self._app = App(self)
         self._gpio = Gpio(self)
         self._sim = Sim(self)
         self._socket = Socket(self)
 
         self._kernelLogDevice = kernelLogDevice
+
+    @property
+    def app(self):
+        """Gets our app
+
+        :param self:
+            Self
+
+        :return App:
+            Our app
+        """
+
+        return self._app
 
     @property
     def gpio(self):
@@ -109,59 +124,3 @@ class SkywireNano(Skywire):
 
         except AtInterface.CommError:
             raise Skywire.AtError(None, "Failed to detect boot")
-
-    @property
-    def versions(self):
-        """Gets our versions
-
-        :param self:
-            Self
-
-        :raise IOError:
-            Failed to get versions
-
-        :return Array of tuples:
-            The version names and their values
-        """
-
-        # Get the stack and AT interface versions
-        response = self.at.sendCommand("AT#APPVER?")
-
-        # If that failed, that's a paddlin'
-        if not response:
-            raise IOError("Failed to get app versions")
-
-        # Get the version lines
-        versions = response.output.split(self.at.NewLine)
-
-        # If there aren't two versions, that's a paddlin'
-        if len(versions) != 2:
-            raise IOError("Failed to get all app versions")
-
-        for i in range(len(versions)):
-            # Each version follows '#APPVER: <thing>: <version>'
-            fields = versions[i].split(":")
-
-            # If that wasn't the case for any version, that's a paddlin'
-            if len(fields) != 3:
-                raise IOError("Invalid app version response")
-
-            # Make sure we remove any whitespace
-            versions[i] = (fields[1].strip(), fields[2].strip())
-
-        # Get the modem/co-processor version
-        response = self.at.sendCommand("AT+CGMR")
-
-        # If that failed, that's a paddlin'
-        if not response:
-            raise IOError("Failed to get co-processor version")
-
-        # If there isn't a single version, that's a paddlin'
-        if len(response.output) < 1:
-            raise IOError("Invalid co-processor version response")
-
-        # The modem version is just a single string, so add our own identifier
-        # for it
-        versions.append(("MFW", response.output))
-
-        return versions
