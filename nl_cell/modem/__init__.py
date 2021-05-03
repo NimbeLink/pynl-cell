7c8c6db75ef38193fe56b51214955c6c02e76aa3
Erik Johnson <joh06937@gmail.com>
2021-05-03 09:01:42 -0500

    Source: Updated our previous Doxygen-like comment header and copyright with Pythonic

--- nl_cell/modem/__init__.py
+++ nl_cell/modem/__init__.py
@@ -1,16 +1,14 @@
-###
- # \file
- #
- # \brief The NimbeLink modem package
- #
- # (C) NimbeLink Corp. 2020
- #
- # All rights reserved except as explicitly granted in the license agreement
- # between NimbeLink Corp. and the designated licensee.  No other use or
- # disclosure of this software is permitted. Portions of this software may be
- # subject to third party license terms as specified in this software, and such
- # portions are excluded from the preceding copyright notice of NimbeLink Corp.
- ##
+"""
+The NimbeLink modem package
+
+(C) NimbeLink Corp. 2020
+
+All rights reserved except as explicitly granted in the license agreement
+between NimbeLink Corp. and the designated licensee. No other use or disclosure
+of this software is permitted. Portions of this software may be subject to third
+party license terms as specified in this software, and such portions are
+excluded from the preceding copyright notice of NimbeLink Corp.
+"""
 
 from nimbelink.cell.modem.app import App
 from nimbelink.cell.modem.atError import AtError
--- nl_cell/network/mode.py
+++ nl_cell/network/mode.py
@@ -1,16 +1,14 @@
-###
- # \file
- #
- # \brief Provides network tools
- #
- # (C) NimbeLink Corp. 2020
- #
- # All rights reserved except as explicitly granted in the license agreement
- # between NimbeLink Corp. and the designated licensee.  No other use or
- # disclosure of this software is permitted. Portions of this software may be
- # subject to third party license terms as specified in this software, and such
- # portions are excluded from the preceding copyright notice of NimbeLink Corp.
- ##
+"""
+Provides network tools
+
+(C) NimbeLink Corp. 2020
+
+All rights reserved except as explicitly granted in the license agreement
+between NimbeLink Corp. and the designated licensee. No other use or disclosure
+of this software is permitted. Portions of this software may be subject to third
+party license terms as specified in this software, and such portions are
+excluded from the preceding copyright notice of NimbeLink Corp.
+"""
 
 class Mode:
     """Network modes
